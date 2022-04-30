ed72a2f5f6ad6f70ad6c810f4a6f161f33dd23ae
Sardor Madaminov <65296404+Sardor-M@users.noreply.github.com>
2022-04-30 23:11:25 +0900

    Update db.js

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ const github = {
   username: "Sardor-M",
   headers: {
     "Content-Type": "application/json",
-    Authorization: // Your Github access token will go here 
+    Authorization: 
   },
 };
 
